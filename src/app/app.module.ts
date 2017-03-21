f9a20a49add80324d43c6fb4b73d9195dd0aba0a
RyoYamada <newfield1001@gmail.com>
2017-03-22 00:52:01 +0900

    add mainpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,32 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 
+import { RouterModule }   from '@angular/router';
+import { MainpageComponent } from './component/mainpage/mainpage.component';
+
+let route = RouterModule.forRoot([
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'mainpage',
+    component: MainpageComponent
+  },
+])
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    route,
     MaterialModule.forRoot()
   ],
   providers: [],
