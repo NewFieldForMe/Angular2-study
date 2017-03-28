67162133aa23539bc10b2b8f13d224180b325db4
RyoYamada <newfield1001@gmail.com>
2017-03-29 00:08:53 +0900

    login api call

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 import { ApiService } from '../../service/api.service';
+import { AuthHttp } from 'angular2-jwt';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,7 @@ export class LoginComponent implements OnInit {
 
   loginId: string
   password: string
+  errorMessage: string
 
   constructor(
     private router: Router,
@@ -18,11 +20,18 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.errorMessage = "";
   }
 
   loginClick(event) {
-    this.apiService.getAuthentication(this.loginId, this.password);
-    //this.router.navigate(['mainpage']);
+    let promise = this.apiService.getAuthentication(this.loginId, this.password);
+    promise.then(response => {
+        console.log(response)
+        this.router.navigate(['mainpage']);
+      })
+      .catch((error:any) => {
+        console.debug()
+        this.errorMessage = "ログインに失敗しました。";
+      });
   }
-
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Headers, Http, Response } from '@angular/http';
 import { AuthHttp } from 'angular2-jwt';
 import 'rxjs/add/operator/toPromise';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ApiService {
@@ -9,26 +10,30 @@ export class ApiService {
 
   constructor(private http: Http, public authHttp: AuthHttp) { }
 
-  getAuthentication(accountId: string, password: string) {
-    console.debug('test');
-    this.http.post('http://localhost:3000/api/authenticate',
+  getAuthentication(accountId: string, password: string) :Promise<Response> {
+    return this.http.post('http://localhost:3000/api/authenticate',
       JSON.stringify({
         "accountid": accountId,
         "password": password
       }), { headers: this.headers })
-      .toPromise()
-      .then(response => {
+      .map((response: Response) => {
         var token = response.json()['auth_token'];
         console.log(token);
         sessionStorage.setItem('auth_token', token);
-        this.authHttp.get('http://localhost:3000/api/sheetrows')
-          .subscribe(
-          data => console.log(data),
-          err => console.log(err),
-          () => console.log('Request Complete')
-          );
-      })
-      .catch(this.handleError);
+        return response
+      }).toPromise()
+      // .then(response => {
+      //   var token = response.json()['auth_token'];
+      //   console.log(token);
+      //   sessionStorage.setItem('auth_token', token);
+      //   this.authHttp.get('http://localhost:3000/api/sheetrows')
+      //     .subscribe(
+      //     data => console.log(data),
+      //     err => console.log(err),
+      //     () => console.log('Request Complete')
+      //     );
+      // })
+      // .catch(this.handleError);
   }
   private handleError(error: any) {
     console.error(error);
